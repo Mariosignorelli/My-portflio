f6699dd9e4a55f69b53a08fb2447b066d89d2c11
Mario Signorelli <Signorelii2308@gmail.com>
2024-01-22 10:48:28 -0300

    create button laguage

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const acordeonTigger = document.querySelectorAll('.acordeon .trigger')
+acordeonTigger.forEach((trigger)=>{
+    trigger.addEventListener("click",(e) =>{
+        const acordeon = trigger.parentElement
+        const isOpen = acordeon.classList.contains('open')
+
+        if(isOpen ){
+                acordeon.classList.remove('open')
+        }else{
+            acordeon.classList.add('open')
+        }
+    })
+} )
